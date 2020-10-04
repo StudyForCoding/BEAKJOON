f76429c4592a02d8252152a38eaf734257eeb3bf
gamjapark <gojang4@gmail.com>
2020-10-05 00:03:33 +0900

    feat(py):change step8 answer

--- 15_Backtracking/Step08/gamjapark.py
+++ 15_Backtracking/Step08/gamjapark.py
@@ -8,19 +8,14 @@ answer = sys.maxsize
 
 for i in range(len(arr) - 1):
 	s, l = 0, 0
-	for j in range(i + 1, len(arr)):
-		ttt = set(arr[j] + arr[i])
-		if len(ttt) != N:
-			continue
-		else:
-			aaa = list(combinations(arr[i], 2))
-			bbb = list(combinations(arr[j], 2))
-			for a in range(len(aaa)):
-				(x, y) = aaa[a]
-				s += (mappp[x][y] + mappp[y][x])
+	aaa = list(combinations(arr[i], 2))
+	bbb = list(combinations(set(team) - set(arr[i]), 2))
+	for a in range(len(aaa)):
+		(x, y) = aaa[a]
+		s += (mappp[x][y] + mappp[y][x])
 
-			for b in range(len(bbb)):
-				(x, y) = aaa[a]
-				l += (mappp[x][y] + mappp[y][x])
-			answer = min(answer, abs(s-l))
+	for b in range(len(bbb)):
+		(x, y) = bbb[b]
+		l += (mappp[x][y] + mappp[y][x])
+	answer = min(answer, abs(s-l))
 print(answer)
