ac625578ddf08cf9b17951f20b5e47022cffc809
gamjapark <gojang4@gmail.com>
2021-02-09 13:14:22 +0900

    feat(py): solve step 5

---
+++ 24_DynamicProgramming2/Step05/gamjapark.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+check = list(map(int, sys.stdin.readline().split()))
+
+result = [[0 for _ in range(15001)] for _ in range(31)]
+
+def solve(cur, weight):
+	if cur > n and result[cur][weight]:
+		return
+	result[cur][weight] = 1;
+
+	solve(cur+1, weight+arr[cur])
+	solve(cur+1, weight)
+	solve(cur+1, abs(weight-arr[cur]))
+	
+solve(0,0)
+for c in check:
+	if c > 15000:
+		print("N", end=" ")
+	elif result[n][c]:
+		print("Y", end=" ")
+	else:
+		print("N", end=" ")
