93e53281898379e951f10b4d40b259f3bb13c8c3
wowo0709 <wjsduddn0709@naver.com>
2021-03-16 23:46:45 +0900

    beakjoon 5639

---
+++ 29_Tree/Step06/wowo0709.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10**9)
+def f(start, end):
+    if start > end:
+        return
+    else:
+        root = pre[start]
+        div = end + 1
+        for pos in range(start+1, end+1):
+            if root < pre[pos]:
+                div = pos
+                break
+        f(start+1, div-1)
+        f(div, end)
+        print(root)
+
+pre = []
+while True:
+    try:
+        pre.append(int(sys.stdin.readline()))
+    except:
+        pre(0,len(pre)-1)
