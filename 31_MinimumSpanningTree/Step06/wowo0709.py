47c3c91a9f7c9f05c52c89a6bfb6bc8aa029870a
wowo0709 <wjsduddn0709@naver.com>
2021-03-31 00:15:21 +0900

    modify 17472

--- 31_MinimumSpanningTree/Step06/wowo0709.py
+++ 31_MinimumSpanningTree/Step06/wowo0709.py
@@ -1,6 +1,12 @@
 import sys
 input = sys.stdin.readline
-
+'''
+*** 풀이 과정 ***
+1. MAP을 입력받고 dfs로 각 섬을 라벨링(노드 번호는 2부터)
+2. 상하/좌우 방향 탐색으로 섬을 연결할 수 있는 간선 추가
+3. mst를 만드는 크루스칼 알고리즘으로 각 섬을 연결 
+-> 하나의 라인에서 두 개 이상의 간선이 추가되는 경우 고려!!!
+'''
 # dfs
 dpos = [(0,1),(0,-1),(-1,0),(1,0)]
 def dfs(x,y,n):
@@ -37,11 +43,11 @@ def kruskal(n,edges):
         if union(a,b): 
             mst.append(w)
         if len(mst) == n-1:
-            # print(*mst,sep='\n')
             return sum(mst) # 연결 성공
     return -1 # 연결 실패
     
 # main
+# 과정 1
 N,M = map(int,input().split()) # 세로,가로
 MAP = []
 nodeNum = 2
@@ -51,34 +57,23 @@ for i in range(N):
         if MAP[i][j] == 1:
             dfs(i,j,nodeNum)
             nodeNum += 1
-
+# 과정 2
 edges = set([])
-# max_cnt = [(N,M),(M,N)] # 좌우방향, 상하방향
-for i in range(N): # 좌우방향 탐색
-    w = 0
-    for j in range(M):
-        if w <= 2 and MAP[i][j] > 1: 
-            fromN = MAP[i][j]
-            w = 1
-        if w > 0 and MAP[i][j] == 0: w += 1
-        if w > 2 and MAP[i][j] > 1: 
-            edges.add((w-1,fromN,MAP[i][j]))
-            fromN = MAP[i][j]
-            w = 1
-for i in range(M): # 상하방향 탐색
-    w = 0
-    for j in range(N):
-        if w <= 2 and MAP[j][i] > 1: 
-            fromN = MAP[j][i]
-            w = 1
-        if w > 0 and MAP[j][i] == 0: w += 1
-        if w > 2 and MAP[j][i] > 1: 
-            edges.add((w-1,fromN,MAP[j][i]))
-            fromN = MAP[j][i]
-            w = 1
-
-#print(*MAP,nodeNum,edges,sep='\n')
+max_cnt = [(N,M,'up-down'),(M,N,'left-right')] # 좌우방향, 상하방향
+for cnt1,cnt2,cmd in max_cnt:
+    for i in range(cnt1):
+        w = 0
+        for j in range(cnt2):
+            if cmd == 'up-down': cur1,cur2 = i,j
+            else: cur1,cur2 = j,i
+            if w <= 2 and MAP[cur1][cur2] > 1:         # 시작점
+                fromN = MAP[cur1][cur2]
+                w = 1
+            if w > 0 and MAP[cur1][cur2] == 0: w += 1  # 중간점
+            if w > 2 and MAP[cur1][cur2] > 1:
+                edges.add((w-1,fromN,MAP[cur1][cur2])) # 끝점(은 또다른 시작점)
+                fromN = MAP[cur1][cur2]
+                w = 1
+# 과정 3
 parent = [-1 for _ in range(nodeNum)]
 print(kruskal(nodeNum-2,list(edges)))
-
- 
